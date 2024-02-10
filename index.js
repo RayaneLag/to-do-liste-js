33437516cc5eae4d7189b3df9a15520b3f6525de
rayane <laggounrayan00@gmail.com>
2024-02-10 06:58:01 +0100

    commit 2

--- index.js
+++ index.js
@@ -9,3 +9,35 @@ if (audio1) {
 } else {
   console.log("non");
 }
+var page1 = document.querySelector(".pic");
+var myH1 = document.querySelector(".myH1");
+var page2 = document.querySelector(".alpha");
+
+myH1.addEventListener("click", function () {
+  appearAlpha();
+  console.log("hello");
+});
+
+function appearAlpha() {
+  var DeathNote = document.querySelector(".myH1");
+  var page1 = document.querySelector(".pic");
+  var page2 = document.querySelector(".alpha");
+  DeathNote.style.left = "0";
+  setTimeout(function () {
+    DeathNote.style.left = "-100%";
+    page1.style.display = "none";
+    page2.style.display = "block";
+  }, 1500);
+}
+
+function appearPic() {
+  var DeathNote = Document.querySelector(".myH1");
+  var page1 = document.querySelector(".pic");
+  var page2 = document.querySelector(".alpha");
+  DeathNote.style.left = "0";
+  setTimeout(function () {
+    DeathNote.style.left = "-100%";
+    page1.style.display = "block";
+    page2.style.display = "none";
+  }, 1500);
+}
