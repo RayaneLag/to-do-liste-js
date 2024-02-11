8692854ceaf8d388cf62c88bc4639b32f37c3aae
rayane <laggounrayan00@gmail.com>
2024-02-11 20:44:18 +0100

    commit X

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const audio1 = background.querySelector("audio");
 
 if (audio1) {
   audio1.play();
-  //   setTimeout(() => {
-  //     audio1.pause();
-  //   }, "40000");
+  // setTimeout(() => {
+  //   audio1.pause();
+  // }, "40000");
 } else {
   console.log("non");
 }
